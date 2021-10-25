a17da80eb447640b25e0dc1e7cd126ddc0585a76
Yehonathan Sharvit <viebel@gmail.com>
2021-10-25 04:47:18 +0300

    Use eval instead of exec to evaluate vimscript code

--- lua/halonot.lua
+++ lua/halonot.lua
@@ -6,20 +6,18 @@ vim.o.winminheight = 0
 vim.g.halonot_key = 'o'
 
 local function num_windows()
-  return vim.api.nvim_exec("echo winnr('$')"
-    ,
-    true)
+  return vim.api.nvim_eval("winnr('$')")
 end
 
 local function close_window(n)
-  if (tonumber(n) > 0) then
+  if (n > 0) then
     vim.cmd(n .. "wincmd c")
   end
 end
 
 local function close_window_if_small(n)
-  local width = tonumber(vim.api.nvim_exec('echo winwidth(' .. n .. ')', true))
-  local height = tonumber(vim.api.nvim_exec('echo winheight(' .. n .. ')', true))
+  local width = vim.api.nvim_eval('winwidth(' .. n .. ')')
+  local height = vim.api.nvim_eval('winheight(' .. n .. ')')
   if ((width <  2) or (height < 2)) then
     close_window(n)
   end
